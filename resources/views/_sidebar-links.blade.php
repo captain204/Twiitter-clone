d0233eb21ae510ea603ac67a7e5026165237b1b8
ramkodgreat <nurudeenakindele8@gmail.com>
2020-05-31 19:18:08 +0100

    main page design

---
+++ resources/views/_sidebar-links.blade.php
@@ -0,0 +1,43 @@
+<ul>
+    <li><a 
+    class="font-bold text-lg mb-4 block"
+    href="/">
+    Home</a></li>
+    <li><a 
+        class="font-bold text-lg mb-4 block"
+
+        href="/explore">
+
+        Explore</a></li>
+    <li><a 
+        class="font-bold text-lg mb-4 block"
+
+        href="#">
+
+        Notifications</a></li>
+
+    <li><a 
+        class="font-bold text-lg mb-4 block"
+
+        href="/messages">
+
+        Messages</a></li>
+
+    <li><a 
+        class="font-bold text-lg mb-4 block"
+        href="/bookmarks">
+        Messages</a></li>
+    
+    <li><a 
+        class="font-bold text-lg mb-4 block"
+        href="/lists">
+        Lists</a></li>
+    <li><a 
+        class="font-bold text-lg mb-4 block"
+        href="/profile">
+        Profile</a></li>
+    <li><a 
+        class="font-bold text-lg mb-4 block"
+        href="/more">
+        More</a></li>    
+</ul>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,15 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img 
+            src="https://i.pravatar.cc/50"
+            alt=""
+            class="rounded-full mr-2"
+            >
+    </div>
+    <div>
+        <h5 class="font-bold mb-4"> John Doe </h5>
+        <p class="text-sm">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <p>
+    </div>
+</div>
